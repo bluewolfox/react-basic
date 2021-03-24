c30d33143dca8f8e11b45e3d4631cf666247b5cd
kws_kipis <kws@kipis.co.kr>
2021-03-24 23:51:12 +0900

    5강마무리

---
+++ 5.가위바위보/client.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import RSPClass from "./RSPClass.jsx";
+import RSP from "./RSP.jsx";
+
+ReactDom.render(<RSP />, document.querySelector("#root"));
